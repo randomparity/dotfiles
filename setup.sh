65a7d247f4363e10410c4c8cd7721581c6adefee
David Christensen <randomparity@gmail.com>
2021-08-21 13:24:37 -0700

    Added fix for system/vim python mismatch, skipping existing backups, overwriting existing git credentials

--- setup.sh
+++ setup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+###########################################################
+# Customizations (Begin)
+###########################################################
+GIT_NEW_NAME="David Christensen"
+GIT_NEW_EMAIL="randomparity@gmail.com"
+###########################################################
+# Customizations (End)
+###########################################################
+
+# ToDo: How to find location of dotfile git repository?
 # Store where the script was called from so we can reference it later
+SCRIPT_NAME="$0"
 SCRIPT_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source COLOR_TABLE
 
@@ -117,49 +128,126 @@ fi
 
 # Known dependencies met, install powerline and symlink config files
 # ToDo: Check for install error here
+POWERLINE_INSTALL="Powerline install"
 if python3 -m pip install --user powerline-status powerline-gitstatus &> /dev/null; then
-  echo -e "${TICK} Powerline install"
+  echo -e "${TICK} $POWERLINE_INSTALL"
 else
-  echo -e "${CROSS} Powerline install"
-  echo "  Run 'python3 -m pip install --user powerline-status powerline-gitstatus' manually"
+  echo -e "${CROSS} $POWERLINE_INSTALL"
+  echo "  Automated install failed, run 'python3 -m pip install --user powerline-status powerline-gitstatus' manually"
+  echo "  then rerun $SCRIPT_NAME when finished to complete install"
   exit 1
 fi
 
-# Symlink powerline into the .config directoy
+# Ensure powerline is installed for python version integrated within vim
+# (RHEL 8.4 uses python3.6 for vim and python3.8 for /usr/bin/python3)
+# (Redhat shows python version on gcc compilation line, Ubuntu shows it on link line)
+SYS_PYTHON_VER=$(python3 --version 2>&1 | grep -Po '(?<=^Python )[0-9]*.[0-9]*(?=.[0-9A-Za-z-]*)')
+VIM_PYTHON_VER=$(vim --version | grep -Po '^Compilation:.*[-/Ia-z]+python\K(3\.\d+)|Linking:.*[-a-z]python\K(3\.\d+)')
+echo -e "${INFO} System python version: python$SYS_PYTHON_VER"
+echo -e "${INFO} Vim python version: python$VIM_PYTHON_VER"
+
+if [ $SYS_PYTHON_VER != $VIM_PYTHON_VER ]; then
+  if python$VIM_PYTHON_VER -m pip install --user powerline-status powerline-gitstatus &> /dev/null; then
+    echo -e "${TICK} $POWERLINE_INSTALL for vim"
+  else
+    echo -e "${CROSS} $POWERLINE_INSTALL for vim"
+    echo "  Automated install failed, run 'python$VIM_PYTHON_VER -m pip install --user powerline-status powerline-gitstatus' manually"
+    echo "  then rerun $SCRIPT_NAME when finished to complete install"
+    exit 1
+  fi
+fi
+
+# Ensure we have a ~/.config directory
 [ ! -d "$HOME/.config" ] && mkdir -p "$HOME/.config"
-# ToDo: Progress message here
-# ToDo: What if an existing file in this location?
-[ -d "$HOME/.config/powerline" ] && mv "$HOME/.config/powerline" "$HOME/.config/powerline.backup"
-# ToDo: Progress message here
-if ln -sf "$SCRIPT_HOME/powerline" "$HOME/.config/powerline"; then
-  echo -e "${TICK} Symlink $SCRIPT_HOME/powerline to $HOME/.config/powerline"
+
+# Backup any existing powerline configuration
+if [ -f "$HOME/.config/powerline.backup" ]; then
+  # echo -e "${CROSS} Found existing powerline backup file, skipping backup"
+  :
+elif [ -L "$HOME/.config/powerline.backup" ]; then
+  # echo -e "${CROSS} Found existing powerline backup symlink, skipping backup"
+  :
 else
-  echo -e "${CROSS} Symlink $SCRIPT_HOME/powerline to $HOME/.config/powerline"
+  [ -d "$HOME/.config/powerline" ] && mv "$HOME/.config/powerline" "$HOME/.config/powerline.backup" && echo -e "${CHECK} Backed up existing powerline config file"
+fi
+
+# Symlink powerline configuration from git source directory
+if [ ! -L $HOME/.config/powerline ]; then
+  if ln -sf "$SCRIPT_HOME/powerline" "$HOME/.config/powerline"; then
+    echo -e "${TICK} Symlink $SCRIPT_HOME/powerline to $HOME/.config/powerline"
+  else
+    echo -e "${CROSS} Symlink $SCRIPT_HOME/powerline to $HOME/.config/powerline"
+    # ToDo: Any error message here?
+  fi
+else
+  echo -e "${INFO} Symlink $HOME/.config/powerline already exists, skipping"
 fi
 
 # Symlink all of our dotfiles to the home directory
-# ToDo: Check for existing backups, request user input to overwrite
-for f in .vimrc .bashrc .bash_aliases .bash_profile .tmux.conf .gdbinit .dircolors;
-do
-  # ToDo: How to handle pre-existing .backup files?
-  [ -e $HOME/$f ] && mv $HOME/$f $HOME/$f.backup
-  if ln -sf $SCRIPT_HOME/$f $HOME/$f; then
-    echo -e "${TICK} Symlink $SCRIPT_HOME/$f to $HOME/$f"
+for f in .vimrc .bashrc .bash_aliases .bash_profile .tmux.conf .gdbinit .dircolors; do
+  # Backup any existing config files if present and not already backed up
+  if [[ -e $HOME/$f && -f $HOME/$f.backup ]]; then
+    # echo -e "${CROSS} Found existing $f backup file, skipping backup"
+    :
+  elif [[ -e $HOME/$f && -L $HOME/$f.backup ]]; then
+    # echo -e "${CROSS} Found existing $f backup symlink, skipping backup"
+    :
+  elif [ -e $HOME/$f ]; then
+    if mv $HOME/$f $HOME/$f.backup; then
+      echo -e "${TICK} Backed up $HOME/$f to $HOME/$f.backup"
+    else
+      echo -e "${CROSS} Failed to backup $HOME/$f to $HOME/$f.backup, skipping"
+    fi
   else
-    echo -e "${CROSS} Symlink $SCRIPT_HOME/$f to $HOME/$f"
+    echo -e "${CROSS} No existing file $f to backup, skipping"
+  fi
+
+  # Symlinking config file from git source directory
+  if [ ! -L $HOME/$f ]; then
+    if ln -sf $SCRIPT_HOME/$f $HOME/$f; then
+      echo -e "${TICK} Symlink $SCRIPT_HOME/$f to $HOME/$f"
+    else
+      echo -e "${CROSS} Symlink $SCRIPT_HOME/$f to $HOME/$f"
+    fi
+  else
+    echo -e "${INFO} Symlink $SCRIPT_HOME/$f already exists, skipping"
   fi
 done
 
 # Add git global configuration settings
 # ToDo: Add error checking here
-git config --global user.email "randomparity@gmail.com"
-git config --global user.name "David Christensen"
-echo -e "${TICK} Git global configuration (David Christensen <randomparity@gmail.com>)"
+# ToDo: Better way to capture this info?
+
+# Don't overwrite an existing configuration
+GIT_CUR_NAME=$(git config --global user.name)
+if [ -z "$GIT_CUR_NAME" ]; then
+  if git config --global user.name "$GIT_NEW_NAME"; then	
+    echo -e "${TICK} Set new git name $GIT_NEW_NAME)"
+  else
+    echo -e "${CROSS} Failed to set git name $GIT_NEW_NAME)"
+  fi
+else
+  echo -e "${INFO} Git name already defined ($GIT_CUR_NAME), skipping"
+fi
+
+GIT_CUR_EMAIL=$(git config --global user.email)
+if [ -z "$GIT_CUR_EMAIL" ]; then
+  if git config --global user.email "$GIT_NEW_EMAIL"; then	
+    echo -e "${TICK} Set new git email $GIT_NEW_EMAIL)"
+  else
+    echo -e "${CROSS} Failed to set git email $GIT_NEW_EMAIL)"
+  fi
+else
+  echo -e "${INFO} Git email already defined ($GIT_CUR_EMAIL), skipping"
+fi
+
+GIT_CUR_NAME=$(git config --global user.name)
+GIT_CUR_EMAIL=$(git config --global user.email)
+echo -e "${TICK} Git global configuration ($GIT_CUR_NAME <$GIT_CUR_EMAIL>)"
 
 # ToDo: Any error checking here?
 
 # ToDo: Source the new configuration now?
 echo -e "${TICK} Setup complete, logout/login to enable changes"
 
-# ToDo: Why does script create ~/src/dotfiles/powerline/powerline symlink?
 # ToDo: What if powerline installed system-wide??  How to handle?
