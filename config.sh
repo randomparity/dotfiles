0da2f158fa088c5dc1ea4b21d7f41682ba0bfa82
David Christensen <randomparity@gmail.com>
2022-02-28 06:36:48 +0000

    Big changes in config.sh script

--- config.sh
+++ config.sh
@@ -5,6 +5,7 @@
 ###########################################################
 # Customizations (Begin)
 ###########################################################
+PYENV_PYTHON_VER="3.9.10"
 GIT_NEW_NAME="David Christensen"
 GIT_NEW_EMAIL="randomparity@gmail.com"
 ###########################################################
@@ -24,23 +25,24 @@ BACKUP_DIR=$SCRIPT_HOME/backups/$NOW
 mkdir -p $LOG_DIR
 mkdir -p $BACKUP_DIR
 
+# Function to write status to the console and a log file
+log() {
+  echo -e "$@" 1> >(tee -a $LOG_FILE) 2> >(tee -a $LOG_FILE >&2)
+  # ToDo: Figure out how to strip ANSI sequences from log file
+  # https://superuser.com/questions/380772/removing-ansi-color-codes-from-text-stream
+  # echo -e "$@" | sed 's/\x1b\[[0-9;]*m//g' | tee -a $LOG_FILE
+  # perl -pe ' s/\e\[[\x30-\x3f]*[\x20-\x2f]*[\x40-\x7e]//g; s/\e[PX^_].*?\e\\//g; s/\e\][^\a]*(?:\a|\e\\)//g; s/\e[\[\]A-Z\\^_@]//g;'
+}
+
 # Function to echo commands to log file as they are executed
 exe() {
   params=("$@")
   # Print the command and parameters to the log file
   printf "%s\t%q" "$(date)" "${params[0]}" >> "$LOG_FILE"
-  printf " %q" "${params[@]:1}" >> "$LOG_FILE"
-  printf "\n" >> "$LOG_FILE"
+  printf " %q" "${params[@]:1}"            >> "$LOG_FILE"
+  printf "\n"                              >> "$LOG_FILE"
   # Execute the command and log the output
-  "${params[@]}" >> $LOG_FILE
-}
-
-# Function to write status to the console and a log file (with ANSI stripped)
-log() {
-  echo -e "$@" > >(tee -a $LOG_FILE) 2> >(tee -a $LOG_FILE >&2)
-  # echo -e "$@" | sed 's/\x1b\[[0-9;]*m//g' | tee -a $LOG_FILE
-  # https://superuser.com/questions/380772/removing-ansi-color-codes-from-text-stream
-  # perl -pe ' s/\e\[[\x30-\x3f]*[\x20-\x2f]*[\x40-\x7e]//g; s/\e[PX^_].*?\e\\//g; s/\e\][^\a]*(?:\a|\e\\)//g; s/\e[\[\]A-Z\\^_@]//g;'
+  "${params[@]}" >> $LOG_FILE 2>&1
 }
 
 OS="unknown"
@@ -93,8 +95,8 @@ elif [[ "$OSTYPE" == "darwin"* ]]; then
 fi
 log "${INFO} Found $OS $VER [$ID]"
 
-[[ ! -f .setup_completed ]] && log "${CROSS} Did you run setup.sh?" && exit 1
-log "${TICK} Checking for pre-requisites"
+CHECK_REQS_TXT="Checking for pre-requisites"
+if [[ -f .setup_completed ]]; then log "${TICK} $CHECK_REQS_TXT"; else log "${CROSS} $CHECK_REQS_TXT"; exit 1; fi
 
 ##############################################################################
 # This section is tricky.  In order to use powerline with VIM we need to 
@@ -105,112 +107,101 @@ log "${TICK} Checking for pre-requisites"
 ##############################################################################
 
 ##############################################################################
-# Install pyenv & a local python version
-PYENV_CHECK="Installing pyenv"
-PYENV_VER="3.9.10"
-if [[ ! -f $HOME/.pyenv/bin/pyenv ]]; then
-  curl https://pyenv.run | bash
-fi
-
-# Verify that the install worked
-if [[ ! -f $HOME/.pyenv/bin/pyenv ]]; then
-  log "${CROSS} $PYENV_CHECK"
-  exit 1
-else
-  log "${TICK} $PYENV_CHECK"
-fi
-
-# Setup the pyenv environment for use in the rest of this script
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
-
-# Ensure the system python is selected
-pyenv global system
-
-##############################################################################
-# Fetch and install a powerline instance that matches VIM's python version
+# Find the version of python used by vim and the distro
 #
-# RHEL 8.4     - VIM uses python3.6 and system uses python3.8
-# Ubuntu 18.04 - System uses python3.6
-# Ubuntu 20.04 - 
+#    Distro    | VIM | System |
+# -------------+-----+--------+
+# Fedora 34    | ??? | 3.9.4  |
+# Fedora 35    | ??? | 3.9.4  |
+# Centos 8     | 3.6 | 3.6.8  |
+# RHEL 8.4     | 3.6 |  3.8   |
+# Ubuntu 18.04 | 3.6 |  3.6   |
+# Ubuntu 20.04 | 
+# Debian 10    |
+# Debian 11    | 3.9 | 3.9.2
 #
-# - RHEL8 shows python version on gcc compilation line
-# - Ubuntu shows python version on link line
-# - Fedora 34 does not show python version in "vim --version" output, assume
-#   it matches system python
-POWERLINE_INSTALL="Installing powerline"
 SYS_PYTHON_VER=$(python3 --version 2>&1 | grep -Po '(?<=^Python )[0-9]*.[0-9]*(?=.[0-9A-Za-z-]*)')
 VIM_PYTHON_VER=$(vim --version | grep -Po '^Compilation:.*[-/Ia-z]+python\K(3\.\d+)|Linking:.*[-a-z]python\K(3\.\d+)')
 log "${INFO} Found system python version: python$SYS_PYTHON_VER"
-if [ -z $VIM_PYTHON_VER ]; then
+if [[ -z $VIM_PYTHON_VER ]]; then
   log "${INFO} Found vim python version   : Unknown"
 else
   log "${INFO} Found vim python version   : python$VIM_PYTHON_VER"
 fi
 
-if [[ ! -z $VIM_PYTHON_VER ]]; then
-  if python$VIM_PYTHON_VER -m pip install --user powerline-status powerline-gitstatus &> /dev/null; then
-    log "${TICK} $POWERLINE_INSTALL for vim"
-  else
-    log "${CROSS} $POWERLINE_INSTALL for vim"
-    echo "  Automated install failed, run 'python$VIM_PYTHON_VER -m pip install --user powerline-status powerline-gitstatus' manually"
-    echo "  then rerun $SCRIPT_NAME when finished to complete install"
-    exit 1
-  fi
-fi
-
-if [[ ! -z $SYS_PYTHON_VER ]]; then
-  if python3 -m pip install --user powerline-status powerline-gitstatus &> /dev/null; then
-    log "${TICK} $POWERLINE_INSTALL for system"
-  else
-    log "${CROSS} $POWERLINE_INSTALL for system"
-    echo "  Automated install failed, run 'python$SYS_PYTHON_VER -m pip install --user powerline-status powerline-gitstatus' manually"
-    echo "  then rerun $SCRIPT_NAME when finished to complete install"
-    exit 1
-  fi
+##############################################################################
+# Install powerline for vim scripting
+POWERLINE_VIM_INSTALL="Installing powerline for vim"
+if [[ -z $VIM_PYTHON_VER ]]; then
+  log "${INFO} $POWERLINE_VIM_INSTALL skipped (unknown VIM python version)"
+else
+  exe python$VIM_PYTHON_VER -m pip install --user powerline-status powerline-gitstatus
+  if [[ $? -eq 0 ]]; then log "${TICK} $POWERLINE_VIM_INSTALL"; else log "${CROSS} $POWERLINE_VIM_INSTALL"; exit 1; fi
 fi
 
 ##############################################################################
-# Install a local python version and set it as default
-log "${TICK} Installing & selecting local python $PYENV_VER"
-exe pyenv install -s $PYENV_VER
-exe pyenv global $PYENV_VER
-exe python3 -V
+# Install powerline for bash shell
+POWERLINE_SYS_INSTALL="Installing powerline for bash"
+exe python$SYS_PYTHON_VER -m pip install --user powerline-status powerline-gitstatus
+if [[ $? -eq 0 ]]; then log "${TICK} $POWERLINE_SYS_INSTALL"; else log "${CROSS} $POWERLINE_SYS_INSTALL"; exit 1; fi
 
 ##############################################################################
-# Fetch and install a copy of pip for our local python version
-PIP_FETCH="Fetching local pip installer"
-if exe wget https://bootstrap.pypa.io/get-pip.py; then
-  # https://bootstrap.pypa.io/pip/3.6/get-pip.py
-  log "${TICK} $PIP_FETCH" | sed 's/\x1b\[[0-9;]*m//g' | tee -a $LOG_FILE
-else
-  log "${CROSS} $PIP_FETCH" | sed 's/\x1b\[[0-9;]*m//g' | tee -a $LOG_FILE
-  exit 1
-fi
-
-PIP_INSTALL="Installing local pip"
-if exe python3 get-pip.py; then
-  log "${TICK} $PIP_INSTALL" | sed 's/\x1b\[[0-9;]*m//g' | tee -a $LOG_FILE
-  rm get-pip.py
-else
-  log "${CROSS} $PIP_INSTALL" | sed 's/\x1b\[[0-9;]*m//g' | tee -a $LOG_FILE
-  rm get-pip.py
-  exit 1
+# Install pyenv
+PYENV_INSTALL_TXT="Installing pyenv"
+if [[ ! -f $HOME/.pyenv/bin/pyenv ]]; then
+  # ToDo: How to log results here
+  curl https://pyenv.run | bash
 fi
 
 ##############################################################################
-# Fetch and install powerline for our local python version
-if python3 -m pip install --user powerline-status powerline-gitstatus &> /dev/null; then
-  log "${TICK} $POWERLINE_INSTALL"
-else
-  # ToDo: Is this accurate?
-  log "${CROSS} $POWERLINE_INSTALL"
-  echo "  Automated install failed, run 'python3 -m pip install --user powerline-status powerline-gitstatus' manually"
-  echo "  then rerun $SCRIPT_NAME when finished to complete install"
-  exit 1
-fi
+# Verify that pyenv is installed
+PYENV_CHECK_TXT="Checking pyenv"
+if [[ -x $HOME/.pyenv/bin/pyenv ]]; then log "${TICK} $PYENV_CHECK_TXT"; else log "${CROSS} $PYENV_CHECK_TXT"; exit 1; fi
+
+##############################################################################
+# Setup pyenv environment
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+
+# https://github.com/pyenv/pyenv/issues/1906
+# eval "$(pyenv init -)"
+eval "$(pyenv init --path)"
+eval "$(pyenv virtualenv-init -)"
+# ToDo: What if pyenv already installed and a non-system python is selected?
+
+##############################################################################
+# Install a local python version
+log "${INFO} Preparing local python $PYENV_PYTHON_VER"
+PYTH_INSTALL_TXT="Installing python $PYENV_PYTHON_VER"
+exe pyenv install -s $PYENV_PYTHON_VER
+if [[ $? -eq 0 ]]; then log "${TICK} $PYTH_INSTALL_TXT"; else log "${CROSS} $PYTH_INSTALL_TXT"; exit 1; fi
+
+##############################################################################
+# Set the default python version
+PYTH_SELECT_TXT="Selecting local python $PYENV_PYTHON_VER"
+exe pyenv global $PYENV_PYTHON_VER
+if [[ $? -eq 0 ]]; then log "${TICK} $PYTH_SELECT_TXT"; else log "${CROSS} $PYTH_SELECT_TXT"; exit 1; fi
+
+##############################################################################
+# Report the current python version
+CURR_PYTHON_VER=$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[0:2])))')
+CURR_PYTHON_TXT="Current python3 is $CURR_PYTHON_VER"
+log "${INFO} $CURR_PYTHON_TXT"
+
+##############################################################################
+# Fetch a copy of pip for our local python version
+# https://bootstrap.pypa.io/pip/get-pip.py
+# https://bootstrap.pypa.io/pip/3.6/get-pip.py
+PIP_FETCH_TXT="Fetching bootstrap pip installer"
+exe wget https://bootstrap.pypa.io/get-pip.py
+if [[ $? -eq 0 ]]; then log "${TICK} $PIP_FETCH_TXT"; else log "${CROSS} $PIP_FETCH_TXT"; exit 1; fi
+
+##############################################################################
+# Install a copy of pip for our local python version
+PIP_INSTALL_TXT="Installing pip locally"
+exe python3 get-pip.py
+if [[ $? -eq 0 ]]; then log "${TICK} $PIP_INSTALL_TXT"; else log "${CROSS} $PIP_INSTALL_TXT"; exit 1; fi
+exe rm get-pip.py.*
 
 ##############################################################################
 # Ensure we have a ~/.config directory
@@ -231,94 +222,57 @@ POWERLINE_SYMLINK_INSTALL="Symlinking $SCRIPT_HOME/powerline to $HOME/.config/po
 if [ -L "$HOME/.config/powerline" ]; then
   log "${INFO} $POWERLINE_SYMLINK_INSTALL skipped, found existing symlink"
 else
-  if ln -sf "$SCRIPT_HOME/powerline" "$HOME/.config/powerline"; then
-    log "${TICK} $POWERLINE_SYMLINK_INSTALL"
-  else
-    log "${CROSS} $POWERLINE_SYMLINK_INSTALL"
-  fi
+  ln -sf "$SCRIPT_HOME/powerline" "$HOME/.config/powerline"
+  if [[ $? -eq 0 ]]; then log "${TICK} $POWERLINE_SYMLINK_INSTALL"; else log "${CROSS} $POWERLINE_SYMLINK_INSTALL"; fi
 fi
 
 ##############################################################################
 # Symlink new dotfiles to the home directory
 for f in .vimrc .bashrc .bash_aliases .bash_profile .tmux.conf .gdbinit .dircolors; do
   DOTFILE_BACKUP_INSTALL="Backing up $HOME/$f to $BACKUP_DIR/$f"
-  DOTFILE_SYMLINK_INSTALL="Creating symlink from $HOME/$f to $SCRIPT_HOME/$f"
+  DOTFILE_SYMLINK_INSTALL="Symlinking $HOME/$f to $SCRIPT_HOME/$f"
   # Backup any existing config files if present and not already backed up
   if [ -e $HOME/$f ]; then
-    if mv $HOME/$f $BACKUP_DIR/$f; then
-      log "${TICK} $DOTFILE_BACKUP_INSTALL"
-    else
-      log "${CROSS} $DOTFILE_BACKUP_INSTALL failed"
-      # ToDo: Any error handling here?
-    fi
+    mv $HOME/$f $BACKUP_DIR/$f
+    if [[ $? -eq 0 ]]; then log "${TICK} $DOTFILE_BACKUP_INSTALL"; else log "${CROSS} $DOTFILE_BACKUP_INSTALL failed"; fi
   fi
 
   # Symlinking config file from git source directory
-  if ln -sf $SCRIPT_HOME/$f $HOME/$f; then
-    log "${TICK} $DOTFILE_SYMLINK_INSTALL"
-  else
-    log "${CROSS} $DOTFILE_SYMLINK_INSTALL failed"
-  fi
+  ln -sf $SCRIPT_HOME/$f $HOME/$f
+  if [[ $? -eq 0 ]]; then log "${TICK} $DOTFILE_SYMLINK_INSTALL"; else log "${CROSS} $DOTFILE_SYMLINK_INSTALL failed"; fi
 done
 
 ##############################################################################
 # Configure git user name
 GIT_CUR_NAME=$(git config --global user.name)
 GIT_NAME_INSTALL="Setting git name ($GIT_NEW_NAME)"
-if [ -z "$GIT_CUR_NAME" ]; then
-  if git config --global user.name "$GIT_NEW_NAME"; then	
-    log "${TICK} $GIT_NAME_INSTALL"
-  else
-    log "${CROSS} $GIT_NAME_INSTALL failed"
-  fi
-else
-  log "${INFO} $GIT_NAME_INSTALL skipped, $GIT_CUR_NAME already defined"
-fi
+git config --global user.name "$GIT_NEW_NAME"
+if [[ $? -eq 0 ]]; then log "${TICK} $GIT_NAME_INSTALL"; else log "${CROSS} $GIT_NAME_INSTALL failed"; fi
 
 ##############################################################################
 # Configure git user email
 GIT_CUR_EMAIL=$(git config --global user.email)
 GIT_EMAIL_INSTALL="Setting git email ($GIT_NEW_EMAIL)"
-if [ -z "$GIT_CUR_EMAIL" ]; then
-  if git config --global user.email "$GIT_NEW_EMAIL"; then	
-    log "${TICK} $GIT_EMAIL_INSTALL"
-  else
-    log "${CROSS} $GIT_EMAIL_INSTALL failed"
-  fi
-else
-  log "${INFO} $GIT_EMAIL_INSTALL skipped, $GIT_CUR_EMAIL already defined"
-fi
-
-GIT_CUR_NAME=$(git config --global user.name)
-GIT_CUR_EMAIL=$(git config --global user.email)
-log "${TICK} Git global configuration ($GIT_CUR_NAME <$GIT_CUR_EMAIL>)"
+git config --global user.email "$GIT_NEW_EMAIL"
+if [[ $? -eq 0 ]]; then	log "${TICK} $GIT_EMAIL_INSTALL"; else log "${CROSS} $GIT_EMAIL_INSTALL failed"; fi
 
 ##############################################################################
 # Configure git "fixline" alias for DPDK
 GIT_FIXLINE_INSTALL="Setting git fixline alias"
-if git config alias.fixline "log -1 --abbrev=12 --format='Fixes: %h (\"%s\")%nCc: %ae'"; then
-  log "${TICK} $GIT_FIXLINE_INSTALL"
-else
-  log "${CROSS} $GIT_FIXLINE_INSTALL failed"
-fi
+git config alias.fixline "log -1 --abbrev=12 --format='Fixes: %h (\"%s\")%nCc: %ae'"
+if [[ $? -eq 0 ]]; then log "${TICK} $GIT_FIXLINE_INSTALL"; else log "${CROSS} $GIT_FIXLINE_INSTALL failed"; fi
 
 ##############################################################################
 # Configure git pull mode
 GIT_PULL_FF_INSTALL="Setting git pull mode to fast-forward"
-if git config --global pull.ff only; then
-  log "${TICK} $GIT_PULL_FF_INSTALL"
-else
-  log "${CROSS} $GIT_PULL_FF_INSTALL failed"
-fi
+git config --global pull.ff only
+if [[ $? -eq 0 ]]; then log "${TICK} $GIT_PULL_FF_INSTALL"; else log "${CROSS} $GIT_PULL_FF_INSTALL failed"; fi
 
 ##############################################################################
 # Configure git default branch name
 GIT_DEF_BRANCH_NAME="Setting git default branch name to main"
-if git config --global init.defaultBranch main; then
-  log "${TICK} $GIT_DEF_BRANCH_NAME"
-else
-  log "${CROSS} $GIT_DEF_BRANCH_NAME failed"
-fi
+git config --global init.defaultBranch main
+if [[ $? -eq 0 ]]; then log "${TICK} $GIT_DEF_BRANCH_NAME"; else log "${CROSS} $GIT_DEF_BRANCH_NAME failed"; fi
 
 log "${TICK} Setup complete, logout/login to enable changes"
 
